b6867238bd661953ff889ccaf0caf341eefc47d1
Prathikantham <kalyani.prathikantham@capgemini.com>
2022-01-10 14:12:13 +0530

    post customer added

---
+++ src/main/java/com/springboot/bankapp/model/UserInfo.java
@@ -0,0 +1,63 @@
+package com.springboot.bankapp.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class UserInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false,unique = true)
+	private String userName;
+	
+	@Column(nullable = false,unique = true)
+	private String passWord;
+	
+	@OneToMany(fetch = FetchType.EAGER)
+	private List<Role> roles;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	
+	
+	
+}
